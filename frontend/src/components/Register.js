bac07a3fc9ef6e3059ccaa4a8bd0ac79bb5773c1
Axel Perossa <adperossa@gmail.com>
2020-05-30 14:36:56 -0300

    add register functionality to Register component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,51 +1,108 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 
-export default (props) =>{
+export default (props) => {
 
-    const [ userName, setUsername ] = useState('')
-    const [ password, setPassword ] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [registerStatus, setRegisterStatus] = useState('');
+  const [registerMsg, setRegisterMsg] = useState('');
 
-    return(
-        <>
-            <Modal show={props.showRegister}
-                   onHide={props.onHideRegister}
-                   title='Registrate'
-                   body={
-                            <>
-                                <Form>
-                                    <Form.Group>
-                                        
-                                        <Form.Label>Ingrese su email</Form.Label>
-                                        
-                                        <Form.Control type="email"
-                                                      placeholder="email@gmail.com"
-                                                      onChange={e => setUsername(e.target.value)}
-                                        />
+  const handleRegister = (ev) => {
+    ev.preventDefault();
 
-                                    </Form.Group>
+    setRegisterMsg("Enviando credenciales...");
 
-                                    <Form.Group>
-                                        
-                                        <Form.Label>Ingrese su contraseña</Form.Label>
-                                        
-                                        <Form.Control type="password"
-                                                      placeholder="Contraseña"
-                                                      onChange={e => setPassword(e.target.value)}
-                                        />
+    fetch(`http://localhost:8080/register`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ username, password, confirmPassword })
+    })
+      .then(res => res.json())
+      .then(data => {
 
-                                    </Form.Group>
-                                </Form>
-                            </>
-                        }
-                   footer={
-                            <Button>
-                                Registrarse
-                            </Button>
-                          }
-            />
-        </>
-    )
+        if (data.success) {
+
+          setRegisterStatus("success");
+          setRegisterMsg(`Usuarix ${username} registrado exitosamente.`);
+          setTimeout(() => {
+            props.onHideRegister();
+          }, 2000);
+
+        } else {
+
+          setRegisterStatus("danger");
+          if (data.status === 409) {
+            setRegisterMsg("Las contraseñas no coinciden.")
+          } else if (data.status === 403) {
+            setRegisterMsg("Usuario y/o contraseña incorrecta.")
+          } else {
+            setRegisterMsg("Ocurrió un error.");
+            console.error(`Error ${data.status}: ${data.message}`);
+          }
+
+        }
+      })
+      .catch(err => { setRegisterStatus("Error en fetch"); }); // aca capturamos errores fuera de lo comun
+  }
+
+  return (
+    <>
+      <Modal show={props.showRegister}
+        onHide={props.onHideRegister}
+        title='Registrate'
+        body={
+          <>
+            <Form id="registerForm" onSubmit={handleRegister}>
+              <Form.Group>
+
+                <Form.Label>Ingrese su email</Form.Label>
+
+                <Form.Control required type="email"
+                  placeholder="email@gmail.com"
+                  onChange={e => setUsername(e.target.value)}
+                />
+
+              </Form.Group>
+
+              <Form.Group>
+
+                <Form.Label>Ingrese su contraseña</Form.Label>
+
+                <Form.Control required type="password"
+                  placeholder="Contraseña"
+                  onChange={e => setPassword(e.target.value)}
+                />
+
+              </Form.Group>
+
+              <Form.Group>
+
+                <Form.Label>Confirme su contraseña</Form.Label>
+
+                <Form.Control required type="password"
+                  placeholder="Confirmar contraseña"
+                  onChange={e => setConfirmPassword(e.target.value)}
+                />
+
+              </Form.Group>
+
+              <Alert variant={registerStatus}>
+                {registerMsg}
+              </Alert>
+
+            </Form>
+          </>
+        }
+        footer={
+          <Button type="submit" form="registerForm">
+            Registrarse
+          </Button>
+        }
+      />
+    </>
+  )
 
 }
