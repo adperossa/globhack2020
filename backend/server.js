812873833e63a87d8298113ef20493567cf3977c
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-05-30 12:29:54 -0300

    Routes login and register

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-const express = require("express");
+const express = require('express');
 const port = 8080;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+//Initialization
 const app = express();
-require('./DB/database');
+  require("./DB/database");
 
+//Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+//Controllers
+const userController = require(".controllers/UserController");
+
+//Routes
+app.post("/login", userController.login);
+app.post("/register", userController.register);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`)
 });
-
