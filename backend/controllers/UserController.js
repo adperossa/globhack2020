1eb367ff9d4f1f7bbd0fa3e108c3108bff61501b
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-05-30 12:19:30 -0300

    tab space fix

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,23 +1,30 @@
-function login(req, res){
+function login(req, res) {
+  let response;
+  if (!req.body.username || !req.body.password) {
+    response = { success: false, message: "User info is not complete" }
+    return response;
+  }
+  response = { succe }
+
+  res.json(response)
+}
+
+function register(req, res) {
 
 }
 
-function register(req, res){
+function changePassword() {
 
 }
 
-function changePassword(){
-
-}
-
-function editUser(req, res){
+function editUser(req, res) {
 
 }
 
 
 module.exports = {
-    login: login,
-    register: register,
-    changePassword: changePassword,
-    editUser: editUser
+  login: login,
+  register: register,
+  changePassword: changePassword,
+  editUser: editUser
 }
