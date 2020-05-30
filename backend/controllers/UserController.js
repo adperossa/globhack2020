3a457c1fc15c3fd5f0c02e9bd7af075155efb716
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-05-30 12:10:59 -0300

    controller methods added

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,23 @@
+function login(req, res){
+
+}
+
+function register(req, res){
+
+}
+
+function changePassword(){
+
+}
+
+function editUser(req, res){
+
+}
+
+
+module.exports = {
+    login: login,
+    register: register,
+    changePassword: changePassword,
+    editUser: editUser
+}
