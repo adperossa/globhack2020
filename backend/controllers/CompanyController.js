74935feae4e5e1245f873b1b86e03030638ebe78
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-05-30 17:41:54 -0300

    company controller fix

--- backend/controllers/CompanyController.js
+++ backend/controllers/CompanyController.js
@@ -1,17 +1,17 @@
 const Company = require('../models/Company');
 
 async function getCompanyList(req, res) {
-  let { companyName } = req.query.companyName;
+  let companyName  = req.query.companyName;
   if (!companyName) {
     return res.status(200).json({ success: false, status: 400, message: "Company name not received" })
   }
-  let companyList = Company.find({ name: companyName });
+  let companyList = await Company.find();
+  companyList = companyList.filter(company => company.name.toUpperCase().includes(companyName.toUpperCase()))
   
   if (companyList.length === 0) {
     return res.status(409).json({ success: false, status: 400, message: "Company does't exist" })
   }
   
-  companyList = companyList.filter(company => company.name.toUpperCase().includes(companyName.toUpperCase()))
   return res.status(200).json({ success: true, status: 200, message: "Request succesfully", companies: companyList });
 }
 
