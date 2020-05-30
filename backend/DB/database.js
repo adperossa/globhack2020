f2827e6a6e59abf85367b86f2ad546f9248c1369
Alan <devaliant94@gmail.com>
2020-05-30 12:46:10 -0300

    db spacing

--- backend/DB/database.js
+++ backend/DB/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology:true,
+	useUnifiedTopology:	true,
 })
 	.then(db => console.log('DB is connected'))
 	.catch(err => console.log(err));
