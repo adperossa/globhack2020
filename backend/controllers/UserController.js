7468ccfe2289389d5762ea094abf83724d84d729
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-05-30 14:32:09 -0300

    status code added into responses

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,34 +4,34 @@ async function login(req, res) {
   const { username, password } = req.body;
   
   if (!username || !password) 
-    return res.json({ success: false, message: "User info is not complete", status: 400 });
+    return res.status(400).json({ success: false, message: "User info is not complete", status: 400 });
 
   const user = await User.find({ username: username, password: password });
 
   if(user.length === 0)
-    return res.json({ success: false, message: "You are not registered", status: 401 });
+    return res.status(401).json({ success: false, message: "You are not registered", status: 401 });
   
-  return res.json({ success: true, message: "Login successfully", status: 200 });
+  return res.status(200).json({ success: true, message: "Login successfully", status: 200 });
 }
 
 async function register(req, res) {
   const { username, password, confirmPassword } = req.body;
 
   if (!username || !password || !confirmPassword )
-    return res.json({ success: false, message: "User info is not complete", status: 200 });
+    return res.status(200).json({ success: false, message: "User info is not complete", status: 200 });
 
   if (password !== confirmPassword ) 
-    return res.json({ success: false, message: "Incorrect confirm password",status: 409});
+    return res.status(409).json({ success: false, message: "Incorrect confirm password",status: 409});
 
   const userExist = await User.find({ username: username })
 
   if(userExist.length > 0) 
-    return res.json({ success: false, message: "User already exist",status: 403 });
+    return res.status(403).json({ success: false, message: "User already exist",status: 403 });
   
   const NewUser = new User({username, password});
   await NewUser.save();
   
-  return res.json({ success: true, message: "User created correctly", status: 200 });
+  return res.status(200).json({ success: true, message: "User created correctly", status: 200 });
 }
 
 function changePassword(req, res) {
