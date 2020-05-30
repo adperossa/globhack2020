1a68fd7d15f8479476d152124b763e4ac3aca51f
Alan <devaliant94@gmail.com>
2020-05-30 12:07:42 -0300

    DB CONNECTION

---
+++ backend/DB/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology:true,
+})
+	.then(db => console.log('DB is connected'))
+	.catch(err => console.log(err));
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require("express");
 const port = 8080;
 
 const app = express();
+require('./DB/database');
+
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`)
