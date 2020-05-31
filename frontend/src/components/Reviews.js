25bb88765f5c774ecdd2dfa77d456dd705cb60c2
anahi-torres <64815870+anahi-torres@users.noreply.github.com>
2020-05-31 10:10:41 -0300

    finish fetch in Reviews component

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -8,7 +8,7 @@ export default () =>{
     console.log(reviews)
 
     useEffect( () => {
-            fetch(`http://localhost:8080/api/companies`, {
+            fetch(`http://localhost:8080/api/reviews`, {
             
             })
             .then( res => res.json())
@@ -18,38 +18,54 @@ export default () =>{
            }   , []
      );
 
-
+     
     
 
     return(
         <>
             <div className="d-flex justify-content-center flex-wrap">
 
-                <Card className="sombra colour-text m-3" style={{ width: '30rem' }}>
-                    <Card.Body>
+                       
+                        {reviews.map( review => {
+                            
+                            return ( 
 
-                        <Card.Title as="h5" className="mb-4 m-1">Empresa</Card.Title>
 
-                        <Dropdown.Divider className="colour-divider m-2" />
+                                <Card className="sombra colour-text m-3" style={{ width: '30rem' }}>
 
-                        <Card.Text className="m-5">
-                            Textarea
+                                    <Card.Body>
 
-                            <ul>
-                                <li>Pregunta uno</li>
-                                <li>Pregunta dos</li>
-                                <li>Pregunta tres</li>
-                            </ul>
-                        </Card.Text>
+                                        <Card.Title as="h5" className="mb-4 m-1">{review.companyName}</Card.Title>
 
-                        <Dropdown.Divider className="colour-divider m-2" />
+                                        <Dropdown.Divider className="colour-divider m-2" />
 
-                        <div className="text-right mt-5">
-                            <Button className="button-orange">Dejá tu review</Button>
-                        </div>
+                                            <Card.Text className="m-5">
+                                                Ventajas y desventajas: {review.summary}
 
-                    </Card.Body>
-                </Card>
+                                                <ul>
+                                                    <li>Calificación del ambiente: {review.questionOne}</li>
+                                                    <li>Calificación de feedback: {review.questionTwo}</li>
+                                                    <li>Calificación de cv: {review.questionThree}</li>
+
+                                                    <li>Calificación global: {review.average}</li>
+                                                </ul>
+                                            </Card.Text>
+
+                                            <Dropdown.Divider className="colour-divider m-2" />
+
+                                            <div className="text-right mt-5">
+                                                <Button className="button-orange">Dejá tu review</Button>
+                                            </div>
+
+                                    </Card.Body>
+   
+                            </Card>
+
+
+                         ) })}
+                   
+
+    
 
             </div>
 
