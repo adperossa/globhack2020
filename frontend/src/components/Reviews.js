9e331c845897441ceaf29b57eb150c19ff39170c
Axel Perossa <adperossa@gmail.com>
2020-05-31 13:21:20 -0300

    add rating stars for reviews

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -1,61 +1,95 @@
 import React from 'react';
 import CompanySummary from './CompanySummary';
-import {Card, Dropdown, Button} from 'react-bootstrap';
+import { Container, Card, CardDeck, Dropdown, Button } from 'react-bootstrap';
+import StarRating from './StarRating';
 
-export default (props) =>{
-
-    
-    return(
-        <>
-            <div className="d-flex justify-content-center flex-wrap">
-
-                <CompanySummary />
-
-                       
-                        {props.reviews.map( review => {
-                            
-                            return ( 
+export default (props) => {
 
 
-                                <Card className="sombra colour-text m-3" style={{ width: '30rem' }}>
+  return (
+      <div className="d-flex justify-content-center flex-wrap">
 
-                                    <Card.Body>
+        {/* <CompanySummary /> */}
 
-                                        <Card.Title as="h5" className="mb-4 m-1">{review.companyName}</Card.Title>
+          {props.reviews.map(review => {
 
-                                        <Dropdown.Divider className="colour-divider m-2" />
+            return (
 
-                                            <Card.Text className="m-5">
-                                                Ventajas y desventajas: {review.summary}
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
 
-                                                <ul>
-                                                    <li>Calificación del ambiente: {review.questionOne}</li>
-                                                    <li>Calificación de feedback: {review.questionTwo}</li>
-                                                    <li>Calificación de cv: {review.questionThree}</li>
+                    {review.summary}
 
-                                                    <li>Calificación global: {review.average}</li>
-                                                </ul>
-                                            </Card.Text>
+                    <ul className="ratingsList">
+                      <div className="ratingItem">
+                        <li>Calificación del ambiente: </li><StarRating rating={review.questionOne} />
 
-                                            <Dropdown.Divider className="colour-divider m-2" />
+                      </div>
+                      <div className="ratingItem">
+                        <li>Calificación de feedback: </li><StarRating rating={review.questionTwo} />
 
-                                            <div className="text-right mt-5">
-                                                <Button className="button-orange">Dejá tu review</Button>
-                                            </div>
+                      </div>
+                      <div className="ratingItem">
+                        <li>Calificación de cv: </li><StarRating rating={review.questionThree} />
 
-                                    </Card.Body>
-   
-                            </Card>
+                      </div>
+                      <div className="ratingItem">
+                        <li>Calificación global: </li><StarRating rating={review.average} />
+
+                      </div>
+
+                    </ul>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-right">
+                  <Button className="button-orange">Dejá tu review</Button>
+                </Card.Footer>
+              </Card>
 
 
-                         ) })}
-                   
 
-    
 
-            </div>
 
-        </>
-    )
+
+
+
+            )
+          }
+
+          )}
+
+
+      </div>
+
+  )
 
 }
+
+      {/* <Card className="sombra colour-text m-3" style={{ width: '25em' }}>
+
+  <Card.Body>
+
+    <Card.Text>
+      Ventajas y desventajas: {review.summary}
+
+      <ul>
+        <li>Calificación del ambiente: {review.questionOne}</li>
+        <li>Calificación de feedback: {review.questionTwo}</li>
+        <li>Calificación de cv: {review.questionThree}</li>
+
+        <li>Calificación global: {review.average}</li>
+      </ul>
+    </Card.Text>
+
+    <Dropdown.Divider className="colour-divider m-2" />
+
+    <div className="text-right mt-5">
+      <Button className="button-orange">Dejá tu review</Button>
+    </div>
+
+  </Card.Body>
+
+</Card> */}
