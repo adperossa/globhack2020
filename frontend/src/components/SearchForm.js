7f81b7164e05c11ca9add753e78308e3c5646c76
Axel Perossa <adperossa@gmail.com>
2020-05-31 09:56:09 -0300

    add SearchForm functionality

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 export default () => {
+
+  const [inputSearch, setInputSearch] = useState("");
+
+  async function handleSubmit (e) {
+    const results = await (await fetch(`http://localhost:8080/companyReview?companyName=${inputSearch}`)).json();
+    console.log(results);
+    setInputSearch("");
+  }
+
 
   return (
     <div className="d-flex justify-content-center mt-5 search-form">
 
       <div className="input-group mb-3" style={{ width: '30rem' }}>
 
-        <Form.Control size="lg" type="text" placeholder="Buscá por empresa" />
+        <Form.Control 
+          size="lg" 
+          type="text" 
+          placeholder="Buscá por empresa" 
+          value={inputSearch}
+          onChange={e => setInputSearch(e.target.value)}
+           />
 
         <div className="input-group-append">
-          <Button className="button-green">Buscar</Button>
+          <Button onClick={handleSubmit} className="button-green">Buscar</Button>
         </div>
 
       </div>
